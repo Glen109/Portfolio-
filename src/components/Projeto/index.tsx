c057b910ad2da59db96d165fa508ebcddb3cf970
Glen109 <gledson10rodial@gmail.com>
2026-03-05 12:34:40 -0300

    criando a seção de projetos e os componentes do mesmo

---
+++ src/components/Projeto/index.tsx
@@ -0,0 +1,17 @@
+import Paragrafo from "../Paragrafo";
+import { Titulo } from "../Titulo/styles";
+import { Card, LinkBotao } from "./styles";
+
+const Projeto = () => {
+  return (
+    <Card>
+      <Titulo>Projeto</Titulo>
+      <Paragrafo tipo="secundario">
+        Projeto feito com Html e outros.
+      </Paragrafo>
+      <LinkBotao>Visualizar</LinkBotao>
+    </Card>
+  )
+}
+
+export default Projeto
--- src/containers/Sobre/Projetos/index.tsx
+++ src/containers/Sobre/Projetos/index.tsx
@@ -1,8 +1,37 @@
+import Projeto from '../../../components/Projeto'
 import Título from '../../../components/Titulo'
+
+import { Lista } from './styles'
 
 const Projetos = () => (
   <section>
     <Título fontSize={16}>Projetos</Título>
+    <Lista>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+    </Lista>
   </section>
 )
 
