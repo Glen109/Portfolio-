3cc8b2c4e81d1a14402d05691af321341d2b168f
Glen109 <gledson10rodial@gmail.com>
2026-07-09 18:17:17 -0300

    adaptando e colocando tema

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -5,14 +5,18 @@ import Título from '../../components/Titulo'
 
 import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
-const Sidebar = () => (
+type Props = {
+  trocaTema: () => void;
+}
+
+const Sidebar = (props: Props) => (
   <aside>
     <SidebarContainer>
       <Avatar />
       <Título fontSize={20}>Gledson Rodrigues</Título>
       <Paragrafo tipo="secundario" fontSize={16}>Glen109</Paragrafo>
       <Descricao tipo='principal' fontSize={12}>Engenheiro Full-Stack Python</Descricao>
-      <BotaoTema>Trocar tema</BotaoTema>
+      <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
     </SidebarContainer>
   </aside>
 )
--- src/containers/Sidebar/styles.ts
+++ src/containers/Sidebar/styles.ts
@@ -4,20 +4,33 @@ import { P } from '../../components/Paragrafo/styles';
 export const Descricao = styled(P)`
   margin-top: 24px;
   margin-bottom: 40px;
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
 `;
 
 export const BotaoTema = styled.button`
   border-radius: 12px;
   padding: 8px;
-  color: #eee;
+  color: ${props => props.theme.corDeFundo};
   font-size: 10px;
   font-weight: bold;
-  background-color: #282A35;
+  background-color: ${props => props.theme.corPrincipal};
   cursor: pointer;
+  @media (max-width: 768px) {
+      border-radius: 16px;
+      padding: 8px;
+      font-size: 15px;
+    }
 `;
 
 export const SidebarContainer = styled.div`
   position: sticky;
   top: 40px;
   left: 0;
+
+  @media (max-width: 768px) {
+      margin-bottom: 40px;
+      text-align: center;
+    }
 `
